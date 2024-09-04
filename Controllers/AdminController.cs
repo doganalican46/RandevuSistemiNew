4f3ace59ca6dc0579e4345771dd52e8d885d7bb2
Ali Can Dogan <76850300+doganalican46@users.noreply.github.com>
2024-09-04 11:48:27 +0300

    randevular page added to the admin page

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -17,6 +17,38 @@ namespace RandevuSistemiNew.Controllers
             return View();
         }
 
+
+        private List<SelectListItem> GetCategoryList()
+        {
+            return db.Bolumler.Where(k => k.Status == true)
+                              .Select(i => new SelectListItem
+                              {
+                                  Text = i.BolumAd,
+                                  Value = i.ID.ToString()
+                              }).ToList();
+        }
+
+        private List<SelectListItem> GetDoktorList()
+        {
+            return db.Users.Where(k => k.Status == true && k.Role == "doktor")
+                           .Select(i => new SelectListItem
+                           {
+                               Text = i.Fullname,
+                               Value = i.ID.ToString()
+                           }).ToList();
+        }
+
+        private List<SelectListItem> GetHemsireList()
+        {
+            return db.Users.Where(k => k.Status == true && k.Role == "hemsire")
+                           .Select(i => new SelectListItem
+                           {
+                               Text = i.Fullname,
+                               Value = i.ID.ToString()
+                           }).ToList();
+        }
+
+
         public ActionResult Ayarlar()
         {
             return View();
@@ -27,10 +59,132 @@ namespace RandevuSistemiNew.Controllers
             return View();
         }
 
+        //---------------------Randevular Started----------------------//
+        //[Authorize]
         public ActionResult Randevular()
         {
+            var randevular = db.Randevular.ToList();
+            return View(randevular);
+        }
+
+        //[Authorize]
+        [HttpGet]
+        public ActionResult YeniRandevular()
+        {
+            ViewBag.Category = GetCategoryList();
+            ViewBag.Doktor = GetDoktorList();
             return View();
         }
+
+        //[Authorize]
+        [HttpPost]
+        public ActionResult YeniRandevular(Randevular y, int selectedCategoryID, int selectedDoktorID)
+        {
+            ViewBag.Category = GetCategoryList();
+            ViewBag.Doktor = GetDoktorList();
+
+            y.BolumID = selectedCategoryID;
+            y.DoktorID = selectedDoktorID;
+
+            db.Randevular.Add(y);
+            db.SaveChanges();
+            return RedirectToAction("Randevular");
+        }
+
+        //[Authorize]
+        public ActionResult RandevularSil(int id)
+        {
+            var randevular = db.Randevular.Find(id);
+            if (randevular != null)
+            {
+                randevular.Status = false;
+                db.SaveChanges();
+            }
+            return RedirectToAction("Randevular");
+        }
+
+        //[Authorize]
+        public ActionResult RandevularGetir(int id)
+        {
+            var randevu = db.Randevular.Find(id);
+            if (randevu == null)
+            {
+                return HttpNotFound();
+            }
+
+            List<SelectListItem> Category = db.Bolumler
+                                              .Where(k => k.Status == true)
+                                              .Select(i => new SelectListItem
+                                              {
+                                                  Text = i.BolumAd,
+                                                  Value = i.ID.ToString(),
+                                                  Selected = (i.ID == randevu.BolumID)
+                                              }).ToList();
+
+            List<SelectListItem> Doktor = db.Users
+                                            .Where(k => k.Status == true && k.Role == "doktor" && k.BolumID == randevu.BolumID)
+                                            .Select(i => new SelectListItem
+                                            {
+                                                Text = i.Fullname,
+                                                Value = i.ID.ToString(),
+                                                Selected = (i.ID == randevu.DoktorID)
+                                            }).ToList();
+
+            ViewBag.Category = Category;
+            ViewBag.Doktor = Doktor;
+
+            return View(randevu);
+        }
+
+        
+
+
+        //[Authorize
+        [HttpPost]
+        public ActionResult RandevularGuncelle(Randevular y)
+        {
+            if (ModelState.IsValid)
+            {
+                var Randevular = db.Randevular.Find(y.ID);
+                if (Randevular != null)
+                {
+                    Randevular.HastaAd = y.HastaAd;
+                    Randevular.HastaMail = y.HastaMail;
+                    Randevular.HastaPhone= y.HastaPhone;
+                    Randevular.BolumID = y.BolumID;
+                    Randevular.DoktorID= y.DoktorID;
+                    Randevular.RandevuTarih = y.RandevuTarih;
+
+                    Randevular.Status= y.Status;
+
+                    db.SaveChanges();
+                    return RedirectToAction("Randevular");
+                }
+                return HttpNotFound();
+            }
+
+            ViewBag.Category = GetCategoryList();
+            ViewBag.Doktor = GetDoktorList();
+
+            return View("RandevularGetir", y);
+        }
+
+        public JsonResult GetDoktorsByCategory(int categoryID)
+        {
+            var doktors = db.Users
+                            .Where(k => k.Status == true && k.Role == "doktor" && k.BolumID == categoryID)
+                            .Select(i => new SelectListItem
+                            {
+                                Text = i.Fullname,
+                                Value = i.ID.ToString()
+                            }).ToList();
+
+            return Json(doktors, JsonRequestBehavior.AllowGet);
+        }
+
+
+
+        //---------------------Randevular End----------------------//
 
         public ActionResult Doktorlar()
         {
@@ -50,29 +204,15 @@ namespace RandevuSistemiNew.Controllers
         [HttpGet]
         public ActionResult YeniUsers()
         {
-            List<SelectListItem> Category = (from i in db.Bolumler.Where(k => k.Status == true).ToList()
-                                             select new SelectListItem
-                                             {
-                                                 Text = i.BolumAd,
-                                                 Value = i.ID.ToString()
-                                             }).ToList();
-            ViewBag.Category = Category;
+            ViewBag.Category = GetCategoryList();
             return View();
         }
         //[Authorize
         [HttpPost]
         public ActionResult YeniUsers(Users y, int selectedCategoryID)
         {
-            List<SelectListItem> Category = (from i in db.Bolumler.Where(k => k.Status == true).ToList()
-                                             select new SelectListItem
-                                             {
-                                                 Text = i.BolumAd,
-                                                 Value = i.ID.ToString()
-                                             }).ToList();
-            ViewBag.Category = Category;
-
+            ViewBag.Category = GetCategoryList();
             y.BolumID = selectedCategoryID;
-
             db.Users.Add(y);
             db.SaveChanges();
             return RedirectToAction("Users");
@@ -128,14 +268,7 @@ namespace RandevuSistemiNew.Controllers
                 return HttpNotFound();
             }
 
-            // Eğer model geçerli değilse kategorileri tekrar doldurup view'yi geri döneriz
-            List<SelectListItem> Category = (from i in db.Bolumler.Where(k => k.Status == true).ToList()
-                                             select new SelectListItem
-                                             {
-                                                 Text = i.BolumAd,
-                                                 Value = i.ID.ToString()
-                                             }).ToList();
-            ViewBag.Category = Category;
+            ViewBag.Category = GetCategoryList();
 
             return View("UsersGetir", y);
         }
